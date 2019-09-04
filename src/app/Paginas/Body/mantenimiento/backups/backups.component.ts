7257761152445ab1895a90b855c2bccf9b3df079
SalatielMG <salatiel.monteroglez@gmail.com>
2019-09-04 14:56:59 -0600

    Metodo BuscarBackupUserCantidad

--- src/app/Paginas/Body/mantenimiento/backups/backups.component.ts
+++ src/app/Paginas/Body/mantenimiento/backups/backups.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Utilerias} from '../../../../Utilerias/Util';
+import {BackupService} from "../../../../Servicios/backup/backup.service";
 
 @Component({
   selector: 'app-backups',
@@ -9,9 +10,12 @@ import {Utilerias} from '../../../../Utilerias/Util';
 export class BackupsComponent implements OnInit {
 
   private email: string = "";
+  private msj = "";
   //public usuarioMntSearch: FormGroup;
 
-  constructor(private util: Utilerias) { }
+  constructor(private util: Utilerias, private backupService: BackupService) {
+    this.buscarBackups();
+  }
 
   ngOnInit() {
     //this.construirFormulario();
@@ -24,16 +28,33 @@ export class BackupsComponent implements OnInit {
 
   public search() {
     console.log("email user:", this.email);
-    if (this.email.length == 0)
+    if (this.email.length == 0) {
       this.util.emailUserMntBackup = "Generales";
-    else {
+      this.buscarBackups();
+    } else {
       if ((this.util.regex_email).exec(this.email)) {
         this.util.emailUserMntBackup = this.email;
+        this.buscarBackups();
       } else {
         this.util.msjToast("Porfavor ingrese un correo valido", "Email no Valido", true);
       }
     }
 
+  }
+  private buscarBackups() {
+    this.msj = "Buscando backups " + ((this.util.emailUserMntBackup = "Generales") ? this.util.emailUserMntBackup: "del usuario : " + this.util.emailUserMntBackup);
+    this.util.crearLoading().then(() => {
+      this.backupService.buscarBackupsUserCantidad(this.util.emailUserMntBackup, 10).subscribe(result => {
+        this.util.detenerLoading();
+        this.util.msjToast(result.msj, result.titulo, result.error);
+        if (!result.error) {
+          this.backupService.mntBackups = result.backups;
+        }
+      }, error => {
+        this.util.detenerLoading();
+        this.util.msjErrorInterno(error);
+      });
+    });
   }
 
 }
--- src/app/Servicios/backup/backup.service.ts
+++ src/app/Servicios/backup/backup.service.ts
@@ -9,19 +9,26 @@ import {Backup} from '../../Modelos/Backup/backup';
 })
 export class BackupService {
   public backups: Backup[];
-  constructor(public http: HttpClient) { }
+  public mntBackups: Backup[];
+
+  constructor(public http: HttpClient) {
+  }
 
   public eliminarBackup(id): Observable<any> {
     /*const  parametro = new HttpParams()
       .append('id', id);*/
-    return this.http.delete(URL + 'eliminarBackup',{params: {id: id}});
+    return this.http.delete(URL + 'eliminarBackup', {params: {id: id}});
   }
 
   public buscarBackups(idUser): Observable<any> {
     this.backups = [];
-    return this.http.get(URL + 'buscarBackups', {params:{idUser: idUser}});
+    return this.http.get(URL + 'buscarBackups', {params: {idUser: idUser}});
   }
 
+  public buscarBackupsUserCantidad(email, cantidad): Observable<any> {
+    this.mntBackups = [];
+    return this.http.get(URL + 'buscarBackupUserCantidad', {params: {email: email, cantidad: cantidad}});
+  }
   public corregirInconsistencia(Tabla): Observable<any> {
     return this.http.get(URL + 'corregirInconsistenciaDatos' + Tabla);
   }
