2ee308058d2bea65e2b708af8386c33c41b139aa
SalatielMG <salatiel.monteroglez@gmail.com>
2019-09-27 10:25:11 -0500

    Avance MNT BACKUPS

--- src/app/Paginas/Header/nav-encode/nav-encode.component.ts
+++ src/app/Paginas/Header/nav-encode/nav-encode.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UsuarioService} from '../../../Servicios/Usuario/usuario.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-nav-encode',
@@ -8,9 +9,27 @@ import {UsuarioService} from '../../../Servicios/Usuario/usuario.service';
 })
 export class NavEncodeComponent implements OnInit {
 
-  constructor(public userService: UsuarioService) { }
+  constructor(public userService: UsuarioService, private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  private eventLogo() {
+    let ruta = this.router.url.split("/");
+    console.log(ruta);
+    switch (ruta[1]) {
+      case "detalleRespaldo":
+        this.router.navigate(["/backups"]);
+        break;
+      case "detalleUsuario":
+        this.router.navigate(["/backups"]);
+        break;
+      default:
+        this.router.navigate(["/home"]);
+        break;
+    }
+
   }
 
 }
