920b81499238b7325a4558dce0effd489cb33475
SalatielMG <salatiel.monteroglez@gmail.com>
2019-10-18 10:17:00 -0500

    Avance Movements Backu

--- src/app/Modelos/movements/movements.ts
+++ src/app/Modelos/movements/movements.ts
@@ -1,21 +1,25 @@
-export interface Movements {
-  id_backup: number,
-  id_account: number,
-  id_category: number,
-  amount: number,
-  sign: string,
-  detail: string,
-  date_record: string,
-  time_record: string,
-  confirmed: number,
-  transfer: number,
-  date_idx: string,
-  day: number,
-  week: number,
-  fortnight: number,
-  month: number,
-  year: number,
-  operation_code: string,
-  picture: string,
-  iso_code: string,
+export class Movements {
+  id_backup: number;
+  id_account: number;
+  id_category: number;
+  amount: number;
+  sign: string;
+  detail: string;
+  date_record: string;
+  time_record: string;
+  confirmed: number;
+  transfer: number;
+  date_idx: string;
+  day: number;
+  week: number;
+  fortnight: number;
+  month: number;
+  year: number;
+  operation_code: string;
+  picture: string;
+  iso_code: string;
+
+  constructor() {
+
+  }
 }
