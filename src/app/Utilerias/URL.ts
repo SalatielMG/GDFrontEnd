742a26c8b0de47fc82ebf3643f51a6c2288ca561
SalatielMG <salatiel.monteroglez@gmail.com>
2019-11-15 10:39:39 -0600

    Avance 1 commit Proyect Version compilada y funcional para MariaDB

--- src/app/Utilerias/URL.ts
+++ src/app/Utilerias/URL.ts
@@ -1,3 +1,3 @@
-//export const URL = "http://localhost/GDBackEnd/";
+export const URL = "http://localhost/GDBackEnd/";
 //export const URL = "https://databasemaintenance.encodemx.com/GDBackEnd/";
-export const URL = "https://gastosdiarios.montezcode.com/GDBackEnd/";
+//export const URL = "https://gastosdiarios.montezcode.com/GDBackEnd/";
